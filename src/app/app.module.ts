9faa845c951a10c752b57fda95f6d67f15eb128d
adda issa abdoul razak <addaissaabdoulrazak@gmail.com>
2023-02-22 14:32:44 +0100

    ajout du composant menu-general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuGeneralComponent } from './shared/components/menu-general/menu-general.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuGeneralComponent,
   ],
   imports: [
     BrowserModule,
