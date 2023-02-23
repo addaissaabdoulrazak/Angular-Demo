015f679febc75663a9db23b37b8748f2519efa1e
adda issa abdoul razak <addaissaabdoulrazak@gmail.com>
2023-02-24 00:47:27 +0100

    recuperation des valeurs saisi dans un input Introduction au decorator @ViewChield

--- src/app/shared/components/menu-general/menu-general.component.ts
+++ src/app/shared/components/menu-general/menu-general.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu-general',
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuGeneralComponent implements OnInit {
 
+  @ViewChild('IdZonedSearch', { static: false })
+   maZoneRecherch! :ElementRef;
+
+   recherche()
+   {
+     console.log(this.maZoneRecherch.nativeElement.value);
+   }
   constructor() { }
 
   ngOnInit(): void {
